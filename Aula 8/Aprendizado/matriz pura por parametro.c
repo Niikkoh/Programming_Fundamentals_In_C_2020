fda483bc39499d588e24a87ea714827b80548973
Nicolas da Veiga Pereira <nicolas.pereira2000vp@gmail.com>
2019-11-19 22:04:42 -0300

    Aula 8

---
+++ Aula 8/Aprendizado/matriz pura por parametro.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+float calculaMedia(float notasAlunos[][2], int n);
+
+int main()
+{
+
+    float notasAlunos[5][2];
+    float media;
+
+    puts("Insira as notas:\n");
+
+    for(int i=0;i<5;i++)
+    {
+        printf("%d aluno:\n",i+1);
+        scanf("%f",&notasAlunos[i][0]);
+        scanf("%f",&notasAlunos[i][1]);
+        printf("\n\n");
+    }
+
+    system("cls");
+    media = calculaMedia(notasAlunos,5);
+    printf("Media dos alunos: %.2f",media);
+
+    return 0;
+}
+
+float calculaMedia(float notasAlunos[][2], int n)
+{
+    float media,nota;
+
+    media = 0;
+    for(int i=0;i<n;i++)
+    {
+        nota = (notasAlunos[i][0] + notasAlunos[i][1])/2.0;
+        media+=nota;
+    }
+
+    media = (float) media/n;
+
+    return media;
+}
